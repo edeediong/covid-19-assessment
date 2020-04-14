c815cb491cd24cf5d90ebb47c076ac2a46914cd1
edeediong <edidiongmichael08@gmail.com>
2020-04-14 15:14:35 +0100

    Checking Testing Suites

--- src/estimator.py
+++ src/estimator.py
@@ -3,6 +3,4 @@ from abstract import Calculate
 
 def estimator(data):
     """Novelty COVID-19 infections estimator"""
-    workings = Calculate(data)
-    data = workings.estimator
-    return data
+    return Calculate(data).estimator
